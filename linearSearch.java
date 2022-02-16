7603993e4ef6ad433b8bc78f2d64d10c99f4be7e
Shaivik Semwal <shaivik1851.cse18@chitkara.edu.in>
2022-02-16 15:34:22 +0530

    change linear search

--- linearSearch.java
+++ linearSearch.java
@@ -5,6 +5,7 @@ public class linearSearch {
     System.out.println("Enter the size of the array");
     int size=sc.nextInt();
     int [] arr = new int[size];
+    System.out.println("Enter the elements of the arra");
     System.out.println("Enter the elements of the array");
     for(int i=0;i<size;i++)
       arr[i]=sc.nextInt();
